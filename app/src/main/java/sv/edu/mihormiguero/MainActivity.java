6b0ae4d91cc352e3dfa6781ee81912f68d0cf602
Marcela-Gomez <marcelasaraigomezcanas@gmail.com>
2025-10-30 23:43:59 -0600

    Splash screen y menu

--- app/src/main/java/sv/edu/mihormiguero/MainActivity.java
+++ app/src/main/java/sv/edu/mihormiguero/MainActivity.java
@@ -1,6 +1,13 @@
 package sv.edu.mihormiguero;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class MainActivity extends AppCompatActivity {
+    private ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imagen = findViewById(R.id.ant);
+        Glide.with(this)
+                .asGif().load(R.drawable.hormiga).into(imagen);
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.splash);
+        imagen.setAnimation(animation);
+
+        Handler manejador = new Handler();
+        manejador.postDelayed(new Runnable() {
+                                  @Override
+                                  public void run() {
+                                      Intent ventana = new Intent(MainActivity.this, inicio.class);
+                                      startActivity(ventana);
+                                      finish();
+                                  }
+                              }
+                , 3000);
     }
 }
---
+++ app/src/main/java/sv/edu/mihormiguero/inicio.java
@@ -0,0 +1,33 @@
+package sv.edu.mihormiguero;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+}
